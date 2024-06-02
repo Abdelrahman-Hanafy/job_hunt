a544a8796e532661a41d540f46221c584bac1f6b
Abdelrahman-Hanafy <Abdelrahman.m.moustafa@gmail.com>
2024-06-02 12:10:34 +0300

    [Update] Add the default values for job model

--- models/job.py
+++ models/job.py
@@ -10,26 +10,26 @@ class job_hunt(models.Model):
     name = fields.Char(string='Position',required=True)
     company = fields.Char(string='Company',required=True)
 
-    apply_date = fields.Date(string='Apply Date')
+    apply_date = fields.Date(string='Apply Date', defualt=fields.Date.today())
     postion_type = fields.Selection([
         ('full_time', 'Full Time'),
         ('part_time', 'Part Time'),
         ('contract', 'Contract'),
         ('internship', 'Internship'),
-    ])
+    ], default='full_time')
 
     applied_at = fields.Selection([
         ('website', 'Website'),
         ('linkedin', 'Linkedin'),
         ('referral', 'Referral'),
         ('other', 'Other'),
-    ])    
+    ], default='website')    
 
     remote = fields.Selection([
         ('remote', 'Remote'),
         ('onsite', 'Onsite'),
         ('hybrid', 'Hybrid'),
-    ])
+    ], default='onsite')
 
     state = fields.Selection([
         ('wishlisted', 'Wishlisted'),
@@ -41,4 +41,4 @@ class job_hunt(models.Model):
         ('hired', 'Hired'),
         ('declined', 'Declined'),
         ('ghosted', 'Ghosted'),
-    ])
+    ], default='wishlisted')
