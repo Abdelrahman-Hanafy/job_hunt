37d473d4986e420c00b8b9935b7d583c6ff8afc5
Abdelrahman-Hanafy <Abdelrahman.m.moustafa@gmail.com>
2024-06-02 11:22:54 +0300

    [Base] Add the base for the module

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Job Hunt",
+
+    'summary': "Personal App to keep track of jobs",
+
+    'author': "Abdelrahman M Hanafy",
+    'website': "https://www.linkedin.com/in/abdelrahman-hanafy/",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+
+    ],
+
+    'application': True,
+    'installable': True,
+}
+
